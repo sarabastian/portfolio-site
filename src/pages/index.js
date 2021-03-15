0eeef2d90d6581791eb267cc9095a0ee00151fcd
sarabastian <seb118@georgetown.edu>
2021-03-14 22:10:08 -0400

    contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Footer from '../components/Footer';
 import Scroll from '../components/Scroll';
 import overLayPic from '../assets/images/overlay.png';
 import { Link } from 'gatsby';
+import { FaReact } from 'react-icons/fa';
 
 const sections = [
   { id: 'one', bg: require('../assets/images/pic01.jpg') },
@@ -27,8 +28,13 @@ const IndexPage = () => (
             <h2>Who I am</h2>
           </header>
           <p>
-            Hey there, I'm Sara, a full-stack software engineer who loves helping build high quality product for teams .
-            When not at the computer, you can find me playing outside - skiing, biking - or likely re-watching The Office and day-dreaming about french pastries.
+            Hi there, I'm Sara, a full-stack engineer who turned a background in ancient and modern languages into a pursuit to learn, and ultimately love, computer languages.
+          
+          I'm most energized when I am helping build high quality product for forward-looking teams and pioneering missions, while also building strong coworking relationships.
+            <br></br>
+            <br></br>
+            I look forward to the countless doors programming opens to continue down this path of life-long learning and self-improvement.
+            When not at the computer, you can find me skiing, reading, road tripping - or on my way to pet your dog.
           </p>
         </div>
         <Scroll type="id" element="two">
@@ -49,24 +55,29 @@ const IndexPage = () => (
             <h2>Stuff I do</h2>
           </header>
           <p>
-            My stack includes Ruby on Rails, React.js, HTML5/CSS, and SQLite. Check me out on GitHub or shoot me an email for a full CV.
+            My stack includes Ruby on Rails, React.js, HTML5/CSS, and SQLite. 
+            Passionate about all areas of development, from user story road-mapping to design, debugging, and implementation.
           </p>
           <ul className="icons-grid">
             <li>
-              <span className="fas fa-gem" />
-              <h3>Ruby on Rails </h3>
+            <span className="icon major fa-diamond"/>
+            
+              <h3>Back-end <br></br>development </h3>
             </li>
             <li>
-              <span className="icon major fa-code" />
-              <h3>React.js</h3>
+          
+            <span className="icon major fa-code" />
+        
+              <h3>Object-Oriented Programming</h3>
             </li>
             <li>
-              <span className="icon major fa-code" />
-              <h3>Web Development</h3>
+              <span className="icon major fa-object-group" />
+              <h3>UI engineering </h3>
             </li>
             <li>
-              <span className="icon major fa-coffee" />
-              <h3>Debugging</h3>
+              <span className="icon major fa-desktop" />
+              <h3>Responsive <br></br>
+                web design</h3>
             </li>
           </ul>
         </div>
@@ -87,24 +98,30 @@ const IndexPage = () => (
           <header className="major">
             <h2>My Work</h2>
           </header>
-         
-            
-            <p>
-            <a href='https://dev-builder.netlify.app/' > dev-builder</a>
+
+
+          <p>
+            <a href='https://github.com/sarabastian/dev-builder' > dev-builder</a>
             <iframe width="400" height="315"
               src="https://www.youtube.com/embed/H8_LGSDINUk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-         
 
-          <div>
-         
-            <a href='https://github.com/sarabastian/RVnB' > RVnB</a>
+
+            <div>
+
+              <a href='https://github.com/sarabastian/RVnB' > RVnB</a>
+            </div>
+            <iframe width="400" height="315"
+              src="https://www.youtube.com/embed/JquRrALK-Dc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <div>
+
+              <a href='https://github.com/sarabastian/skinssentials-app' > skinssentials</a>
             </div>
             <iframe width="400" height="315" 
-            src="https://www.youtube.com/embed/JquRrALK-Dc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-         
+            src="https://www.youtube.com/embed/ifMWvp_605M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </p>
-       
+
         </div>
         <Scroll type="id" element="footer">
           <a href="#footer" className="goto-next ">
@@ -117,15 +134,15 @@ const IndexPage = () => (
     <section id="footer">
       <div className="container">
         <header className="major">
-          <h2>Get in touch</h2>
+          <h2>Let's chat! Shoot me a message for inquiries, a full resume, or just to say hi. </h2>
         </header>
-        <form method="post" action="#">
+        <form method="post" action="https://formspree.io/f/xvovevve">
           <div className="row gtr-uniform">
             <div className="col-6 col-12-xsmall">
               <input type="text" name="name" id="name" placeholder="Name" />
             </div>
             <div className="col-6 col-12-xsmall">
-              <input type="email" name="email" id="email" placeholder="Email" />
+              <input type="email" name="_replyto" id="email" placeholder="Email" />
             </div>
             <div className="col-12">
               <textarea
@@ -148,7 +165,7 @@ const IndexPage = () => (
             </div>
           </div>
         </form>
-        <Link to="/Elements"> Check out Elements page</Link>
+        {/* <Link to="/Elements"> Check out Elements page</Link> */}
       </div>
       <Footer />
     </section>
